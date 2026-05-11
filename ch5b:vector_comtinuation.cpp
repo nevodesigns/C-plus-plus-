543b3f49dc1e03b4983445d65c9501b5b5e03b6c
Nwokolo Victor Oluebubechukwu <nwokoloebube2l@gmail.com>
2026-05-11 20:07:02 +0100

    explored the strings and its operations

--- ch5b:vector_comtinuation.cpp
+++ ch5b:vector_comtinuation.cpp
@@ -25,5 +25,60 @@ std::cout<<maxNum(data)<<std::endl;
 std::cout<<minNum(data)<<std::endl;
 
 
+
+// moving on to string operation
+
+std::string topic{"roversensor lidar"};
+
+std::cout<<"length: "<<topic.length()<<std::endl;
+std::cout<<"Capacity: "<<topic.capacity()<<std::endl;
+std::cout<<"size: "<<topic.size()<<std::endl;
+std::cout<<"max size: "<<topic.max_size()<<std::endl;
+std::cout<<"empty: "<<topic.empty()<<std::endl;
+std::cout<<"front: "<<topic.front()<<std::endl;
+std::cout<<"back: "<<topic.back()<<std::endl;
+std::cout<<"data: "<<topic.data()<<std::endl;
+
+//accessing individual character
+std::cout<<"first character: "<<topic.at(0)<<std::endl; 
+//we can also do
+std::cout<<"first character: "<<topic[0]<<std::endl;
+
+//cocatenation
+std::string name{"robo"};
+std::string id{"123"};
+std::string nameid{name+id};
+std::cout<<"nameid: "<<nameid<<std::endl;
+
+//checking for a specific substring
+std::cout<<topic.find("sensor")<<std::endl;
+
+//extracting a specific portion
+std::cout<<topic.substr(0,6)<<std::endl; // 0 is theindex 6 is the length
+
+//we can replace
+
+topic.replace(5,10,"status");//iterates to 5 then starts from  6 and removes 10 character
+std::cout<<topic<<std::endl; 
+
+// we can compare
+std::string a {"a"};
+std::string b {"b"};
+if(a==b)std::cout<<"a and b are equal"<<std::endl;
+else if(a!=b)std::cout<<"a and b are not equal"<<std::endl;
+
+
+//
+
+
+
+
+
+
+
+
+
+
 return 0;
 }
+
